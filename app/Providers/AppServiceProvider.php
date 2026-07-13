c456025fd213ca95c759dab7d57bc06fec2e8322
Oussama-Ait-Youss <alikhay815@gmail.com>
2026-07-13 16:28:30 +0200

    feat: install the front - end react project part

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
